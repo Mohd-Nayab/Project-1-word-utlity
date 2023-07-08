798f90836e4205bf9364cf4a218fa7672723fe70
Mohd-Nayab <nayab11nov@gmail.com>
2023-07-08 14:05:32 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <button className="btn btn-primary">button</button>
+    </>
+  );
+}
+
+export default App;
