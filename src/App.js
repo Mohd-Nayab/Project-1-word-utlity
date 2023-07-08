d0010d1b4e1a85ed22c1af22d8540ffc0adfb7b9
Mohd-Nayab <nayab11nov@gmail.com>
2023-07-08 23:26:35 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import React from "react";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
+import About from "./components/About";
 
 const App = () => {
   return (
     <div>
-      <Navbar title="Text-Editor" tab1="Main" />
-      <TextForm formtitle="Enter Your Text" />
+      <Navbar title="Text-Editor" tab1="Home" />
+      {/* <TextForm formtitle="Enter Your Text" /> */}
+      <About />
     </div>
   );
 };
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -51,11 +51,11 @@ const Textform = (props) => {
           Avg Time to read above Content ={0.008 * text.split(" ").length}{" "}
           minutes
         </p>
-        <div class="accordion" id="accordionExample">
-          <div class="accordion-item  ">
-            <h2 class="accordion-header " id="headingOne">
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item  ">
+            <h2 className="accordion-header " id="headingOne">
               <button
-                class="accordion-button"
+                className="accordion-button"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#collapseOne"
@@ -67,11 +67,11 @@ const Textform = (props) => {
             </h2>
             <div
               id="collapseOne"
-              class="accordion-collapse collapse show"
+              className="accordion-collapse collapse show"
               aria-labelledby="headingOne"
               data-bs-parent="#accordionExample"
             >
-              <div class="accordion-body">
+              <div className="accordion-body">
                 <strong>{text}</strong>
               </div>
             </div>
